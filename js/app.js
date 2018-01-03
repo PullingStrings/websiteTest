2dba8c3350f85c96f925398d6dc580b660a11b5b
PullingStrings <drammatik@gmail.com>
2018-01-03 11:41:02 +0000

    still messing with map

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
 $(() => {
 
-  const button = $('button');
+  const button = $('.button');
+  const countryList = $('a');
+
+  countryList.on('click', () => {
+    console.log('here you are');
+  });
 
   button.on('click',() => {
     console.log('you have clicked');
   });
 
   $('#map-africa').CSSMap({
-    'size': 1450,
-    'cities': true
+    'size': 650,
+    'cities': true,
+    'mapStyle': 'black',
+    'agentsList': {
+      'enable': true,
+      'agentsList': '#kenya'
+    }
   });
 
 });
